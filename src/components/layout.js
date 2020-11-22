c776402c49c2289e4f1c18aa787be96346273d28
dfaiv <faivre@gmail.com>
2020-11-21 19:51:53 -0600

    remove bio, move logo to `layout.js`

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,22 +1,64 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link, useStaticQuery } from "gatsby"
+import Image from "gatsby-image"
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
   let header
 
+  const data = useStaticQuery(graphql`
+    query LayoutQuery {
+      avatar: file(absolutePath: { regex: "/profile-pic.png/" }) {
+        childImageSharp {
+          fixed(width: 50, height: 50, quality: 95) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+      site {
+        siteMetadata {
+          author {
+            name
+            summary
+          }
+          social {
+            twitter
+          }
+        }
+      }
+    }
+  `)
+
+  const author = data.site.siteMetadata?.author
+  const avatar = data?.avatar?.childImageSharp?.fixed
+
+  const logo = (avatar && (
+    <Image
+      fixed={avatar}
+      alt={author?.name || ``}
+      className="bio-avatar"
+      imgStyle={{
+        borderRadius: `50%`
+      }}
+    />
+  ))
+
   if (isRootPath) {
     header = (
       <h1 className="main-heading">
+        <div>{logo}</div>
         <Link to="/">{title}</Link>
       </h1>
     )
   } else {
     header = (
-      <Link className="header-link-home" to="/">
-        {title}
-      </Link>
+      <div>
+        <div>{logo}</div>
+        <Link className="header-link-home" to="/">
+          {title}
+        </Link>
+      </div>
     )
   }
 
