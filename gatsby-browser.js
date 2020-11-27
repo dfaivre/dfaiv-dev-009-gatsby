02eab0661d2dce5f482f2bf8c9beda832c643c4f
dfaiv <faivre@gmail.com>
2020-11-27 12:59:03 -0600

    STYLE - use `fontsource` instead of depricated `typeface`

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,13 @@
 // custom typefaces
-import "typeface-montserrat"
-import "typeface-merriweather"
+
+import "fontsource-montserrat/200.css"
+import "fontsource-montserrat/300.css"
+import "fontsource-montserrat/400.css"
+import "fontsource-montserrat/700.css"
+
+import "fontsource-merriweather/400.css"
+
+
 // normalize CSS across browsers
 import "./src/normalize.css"
 // custom CSS styles
